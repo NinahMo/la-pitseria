51112528436ed021a811ac1751de7c0cf428b589
unknown <ninahb746@gmail.com user.name NinahMo>
2020-05-18 06:33:32 +0300

    Add link elements that link javascript and jquery to the site

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+// bussiness logic
+
+function TotalAmount(size, crust, toppings) {
+    this.sizeName = size;
+    this.crustName = crust;
+    this.toppingsName = toppings;
+}
+    var pizzaSize;
+    var pizzaCrust;
+    var pizzaToppings=[];
+    var delivery;
+    var totaltopping;
+    
+    function getToppingPrice(){
+      return pizzaToppings.length * 100
+    };
+    function getTotal(){
+        return pizzaCrust + pizzaSize + delivery + getToppingPrice();
+    };
 function makeFullOrder()
 {
   var checkoutName = getCookie("name");
